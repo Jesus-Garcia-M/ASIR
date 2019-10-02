5752d844afdfab6d0e811d2b6a49af51e359ea1d
Jesús García <jesus.garcia.inf@gmail.com>
2019-10-02 17:54:27 +0200

    Terminado Script

--- ASO/ListarPaquetes.sh
+++ ASO/ListarPaquetes.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
+# Uso: Lista los paquetes instalados de un repositorio, necesita de 2 parámetros, el nombre del repositorio y la "rama" en caso de que
+# el repositorio indicado tenga varias. (Ej: deb.debian.org_debian_dists_buster/buster-backports/buster-updates)
 
-for paqrepo in $(grep Package /var/lib/apt/lists/$1|cut -d' ' -f2)
-	do
-		paqueterepo=$paqrepo
-		for paqinst in `dpkg -l | grep ^ii | awk '{print $2}'`
+if [ -z "$2" ]
+	then
+		# Recorre el repositorio indicado y guarda el nombre del paquete en una variable.
+		for paqrepo in $(grep Package /var/lib/apt/lists/$1*Packages|cut -d' ' -f2)
 			do
-				if [ $paqinst == $paqueterepo ]
-				then
-					echo $paqinst;
-				fi
+				paqueterepo=$paqrepo
+				# Recorre los paquetes instalados y comprubea si corresponden al repositorio, si corresponde
+				# lo imprime por pantalla.
+				for paqinst in `dpkg -l |grep ^ii |awk '{print $2}'`
+					do
+						if [ $paqinst == $paqueterepo ]
+							then
+								echo $paqinst;
+						fi
+					done
 			done
-	done
+	else
+		for paqrepo in $(grep Package /var/lib/apt/lists/$1*$2*Packages|cut -d' ' -f2)
+			do
+				paqueterepo=$paqrepo
+				for paqinst in `dpkg -l |grep ^ii |awk '{print $2}'`
+					do
+						if [ $paqinst == $paqueterepo ]
+							then
+								echo $paqinst;
+						fi
+					done
+			done
+fi
